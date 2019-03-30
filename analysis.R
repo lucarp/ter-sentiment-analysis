d94aa0958b496a5300b68e4c14d3342035c7052d
Mathieu Pont <matthieu.pont@hotmail.fr>
2019-03-30 20:10:15 +0100

    add WC-NMTF

--- analysis.R
+++ analysis.R
@@ -1,29 +1,61 @@
 setwd("/media/matthieu/Data/Matthieu/##Etude/#M1/S2/TER/ter-sentiment-analysis")
 
+library(aricode)
 library(R.matlab)
+library(skmeans)
+library("FactoMineR")
+library(NMF)
+
+# ------- Dataset loading -------
 #X <- readMat("mat_files/output_30.csv_tf-idf-l2.mat")
 #X <- readMat("mat_files/output_not_original_30.csv_bow.mat")
 #X <- readMat("mat_files/output_not_original_30.csv_tf-idf.mat")
-X <- readMat("mat_files/output_not_original_30.csv_tf-idf-l2.mat")
-X <- readMat("mat_files/output_not_original_50.csv_tf-idf-l2.mat")
+#X <- readMat("mat_files/output_not_original_30.csv_tf-idf-l2.mat")
+#X <- readMat("mat_files/output_not_original_50.csv_tf-idf-l2.mat")
 X <- readMat("mat_files/output_not_original_10.csv_tf-idf-l2.mat")
+#X <- readMat("mat_files/output_not_original_5.csv_tf-idf-l2.mat")
+#X <- readMat("mat_files/output_not_original_no_clean.csv_tf-idf-l2.mat")
+#X <- readMat("mat_files/output_not_original_100.csv_tf-idf-l2.mat")
 df <- X$X
-df
+#df
+dim(df)
+mat_df <- as.matrix(df)
+dim(mat_df)
 label <- read.csv("mat_files/dataset_LABEL.csv", header = FALSE)
-label
+#label
+# ----------------------------------------
 
-# --- Split, if wanted, per autor ---
+svds(df)
+
+
+# ------- Split, if wanted, per autor -------
+# Get ID for each author
+temp <- label$V1[1]
+for(i in 2:length(label$V1)) { 
+  temp <- label$V1[i-1]
+  if(label$V1[i] < temp)
+    print(i)
+}
+#1028
+#2335
+#3237
+
 label <- matrix(label[1:1027,])
 df <- df[1:1027,]
 label <- matrix(label[1028:2334,])
 df <- df[1028:2334,]
 label <- matrix(label[2335:3236,])
 df <- df[2335:3236,]
-#label <- matrix(label[3237:,])
-#df <- df[3237:,]
-# -----------------------------------
+#label <- matrix(label[3237:length(label),])
+#df <- df[3237:length(label),]
+# ----------------------------------------
 
-# --- K-means ---
+
+k <- 5
+labelK <- apply(label, MARGIN = 1, FUN=function(x) max(1, ceiling(x*k))) # true label (1 to k)
+
+
+# ------- K-means -------
 # - Elbow criteron
 rng <- 2:10 #K from 2 to 20
 tries <- 10 #Run the K Means algorithm x times
@@ -49,24 +81,24 @@ plot(rng, avg.rsquared, type="b", main="R squared by Various K",
      ylab="Average R squared",
      xlab="Value of K")
 
-
-k <- 5
+# - K means clustering
 res <- kmeans(df, centers = k)
-labelK <- apply(label, MARGIN = 1, FUN=function(x) max(1, ceiling(x*k)))
 length(res$cluster)
 length(labelK)
 
-# TODO - Do correclty the NMI and ARI (label value)
-library(aricode)
-NMI(res$cluster, labelK)
-ARI(res$cluster, labelK)
-
+# - Plot 
 layout(matrix(1:2))
 plot(labelK)
 plot(res$cluster)
 
-# --- Spherical K-means ---
-library(skmeans)
+# - Compute NMI and ARI
+# TODO - Do correclty the NMI and ARI (match cluster value with label value)
+NMI(res$cluster, labelK)
+ARI(res$cluster, labelK)
+# ----------------------------------------
+
+
+# ------- Spherical K-means -------
 res2 <- skmeans(df, k)
 res2$cluster
 
@@ -74,21 +106,29 @@ layout(matrix(1:2))
 plot(labelK)
 plot(res2$cluster)
 
+NMI(res2$cluster, labelK)
+ARI(res2$cluster, labelK)
+# ----------------------------------------
 
+
+# ------- PCA -------
+
+layout(matrix(c(1,2), ncol=2))
+resPCA <- PCA(mat_df, scale.unit = FALSE)
+plot.PCA(resPCA, choix="ind", habillage = 9, label = "none")
+
+# ----------------------------------------
+
+
+# ------- NMF -------
+res_nmf <- nmf(mat_df, 4000)
+
+# ----------------------------------------
+
+
+
+# ----------------------------------------
 
 #library(cluster)
 #library(factoextra)
 #fviz_cluster(res, data = df)
-
-
-
-# --- Get ID for each author ---
-temp <- label$V1[1]
-for(i in 2:length(label$V1)) { 
-  temp <- label$V1[i-1]
-  if(label$V1[i] < temp)
-    print(i)
-}
-#1028
-#2335
-#3237
---
+++ nmtf.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+def compute_loss(X, M, Z, S, W, Q, l_reg):
+	ZSW_T = np.dot(np.dot(Z, S), np.transpose(W))
+	WQ_T = np.dot(W, np.transpose(Q))
+	
+	return 1/2 * np.linalg.norm(X - ZSW_T) ** 2 + l_reg/2 * np.linalg.norm(M - WQ_T) ** 2
+
+def wc_nmtf(X, M, g = 5, m = 5, l_reg = 1):
+	n = X.shape[0]
+	d = X.shape[1]
+	
+	Z = np.random.rand(n, g)
+	S = np.random.rand(g, m)
+	W = np.random.rand(d, m)
+	Q = np.random.rand(d, m)
+	
+	X_T = np.transpose(X)
+	M_T = np.transpose(M)
+	
+	i = 0
+	stop_criterion = False
+	while not stop_criterion:
+		loss = compute_loss(X, M, Z, S, W, Q, l_reg)
+		print(loss)
+	
+		# Compute Z
+		S_T = np.transpose(S)
+		W_T = np.transpose(W)
+		delta_Z = np.divide(np.dot(np.dot(X, W), S_T),
+							np.dot(np.dot(np.dot(np.dot(Z, S), W_T), W), S_T)
+							)
+		Z = np.multiply(Z, delta_Z)
+		
+		# Compute W
+		Z_T = np.transpose(Z)
+		Q_T = np.transpose(Q)
+		delta_W = np.divide(np.dot(np.dot(X_T, Z), S) + l_reg * np.dot(M, Q),
+							np.dot(W, np.dot(np.dot(np.dot(S_T, Z_T), Z), S) + l_reg * np.dot(Q_T, Q))
+							)
+		W = np.multiply(W, delta_W)
+
+		# Compute S
+		delta_S = np.divide(np.dot(np.dot(Z_T, X), W),
+							np.dot(np.dot(np.dot(np.dot(Z_T, Z), S), W_T), W)
+							)
+		S = np.multiply(S, delta_S)
+		
+		# Compute Q
+		delta_Q = np.divide(np.dot(M_T, W),
+							np.dot(np.dot(Q, W_T), W)
+							)
+		Q = np.multiply(Q, delta_Q)
+		
+		i += 1
+		stop_criterion = i > 200
+	
+	return {"Z": Z, "S": S, "W": W, "Q": Q}
+	
+if __name__ == '__main__':
+	n = 10
+	d = 5
+	X = np.random.rand(n, d)
+	M = np.random.rand(d, d)	
+	wc_nmtf(X, M)
