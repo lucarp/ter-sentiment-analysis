9f5bffc1da1b46c687181988253620e48922f441
Lucas <lucarp@lucarp.com>
2019-04-06 15:26:18 +0000

    fix nmtf

--- wcnmtf.py
+++ wcnmtf.py
@@ -17,47 +17,38 @@ def wc_nmtf(X, M, g, m, l_reg = 1):
 	n = X.shape[0]
 	d = X.shape[1]
 	
-	Z = np.random.uniform(low=epsilon, size=(n, g))
-	S = np.random.uniform(low=epsilon, size=(g, m))
-	W = np.random.uniform(low=epsilon, size=(d, m))
-	Q = np.random.uniform(low=epsilon, size=(d, m))
-	
-	X_T = np.transpose(X)
-	M_T = np.transpose(M)
+	Z = np.random.rand(n, g)
+	S = np.random.rand(g, m)
+	W = np.random.rand(d, m)
+	Q = np.random.rand(d, m)
 	
 	i = 0
-	epoch = 201
+	epoch = 500
+	print_loss_frequency = epoch / 100
 	stop_criterion = False
 	while not stop_criterion:
-		loss = compute_loss(X, M, Z, S, W, Q, l_reg)
-		print(loss)
+		if i % print_loss_frequency == 0:
+			loss = compute_loss(X, M, Z, S, W, Q, l_reg)
+			print(i,"___",loss)
 	
 		# Compute Z
-		S_T = np.transpose(S)
-		W_T = np.transpose(W)
-		denom = np.dot(np.dot(np.dot(np.dot(Z, S), W_T), W), S_T)
-		denom[denom == 0] = epsilon
-		delta_Z = np.divide(np.dot(np.dot(X, W), S_T), denom)
+		denom = np.dot(np.dot(np.dot(Z, S), np.dot(W.T, W)), S.T)
+		delta_Z = np.divide(np.dot(np.dot(X, W), S.T) + epsilon, denom + epsilon)
 		Z = np.multiply(Z, delta_Z)
 		
 		# Compute W
-		Z_T = np.transpose(Z)
-		Q_T = np.transpose(Q)
-		denom = np.dot(W, np.dot(np.dot(np.dot(S_T, Z_T), Z), S) + l_reg * np.dot(Q_T, Q))
-		denom[denom == 0] = epsilon		
-		delta_W = np.divide(np.dot(np.dot(X_T, Z), S) + l_reg * np.dot(M, Q), denom)
+		denom = np.dot(W, np.dot(np.dot(S.T, np.dot(Z.T, Z)), S) + l_reg * np.dot(Q.T, Q))
+		delta_W = np.divide(np.dot(np.dot(X.T, Z), S) + l_reg * np.dot(M, Q) + epsilon, denom + epsilon)
 		W = np.multiply(W, delta_W)
 
 		# Compute S
-		denom = np.dot(np.dot(np.dot(np.dot(Z_T, Z), S), W_T), W)
-		denom[denom == 0] = epsilon		
-		delta_S = np.divide(np.dot(np.dot(Z_T, X), W), denom)
+		denom = np.dot(np.dot(np.dot(Z.T, Z), S), np.dot(W.T, W))
+		delta_S = np.divide(np.dot(np.dot(Z.T, X), W) + epsilon, denom + epsilon)
 		S = np.multiply(S, delta_S)
 		
 		# Compute Q
-		denom = np.dot(np.dot(Q, W_T), W)
-		denom[denom == 0] = epsilon		
-		delta_Q = np.divide(np.dot(M_T, W), denom)
+		denom = np.dot(Q, np.dot(W.T, W))
+		delta_Q = np.divide(np.dot(M.T, W) + epsilon, denom + epsilon)
 		Q = np.multiply(Q, delta_Q)
 		
 		i += 1
@@ -66,22 +57,22 @@ def wc_nmtf(X, M, g, m, l_reg = 1):
 	return {"Z": Z, "S": S, "W": W, "Q": Q}
 	
 if __name__ == '__main__':
-	"""n = 10
-	d = 20
+	"""n = 5000
+	d = 10000
 	X = np.random.rand(n, d)
 	M = np.random.rand(d, d)"""
 	
 	mat = io.loadmat(sys.argv[1])
 	X = scipy.sparse.csr_matrix.todense(mat['X'])
 	M = term_sentiment_matrix_to_context_matrix(sys.argv[2])
-	
+
 	g = int(sys.argv[3])
 	m = int(sys.argv[4])
 	l_reg = float(sys.argv[5])
 
 	print(X.shape)
 
-	res = wc_nmtf(X, M, g, m, l_reg = l_reg)	
+	res = wc_nmtf(X, M, g, m, l_reg = l_reg)
 	
 	Z = res["Z"]
 	pd.DataFrame(Z).to_csv('wc-nmtf_Z.csv', index=False)
